d0d0fe7c005a12cacbf4d52148ff2c45591e1ca2
John Stendardo <johnakasquib96@gmail.com>
2020-06-07 02:09:12 -0400

    added dropdown and buttons, now just gotta get them to work

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import Table from "./components/Table";
-import TableRow from "./components/TableRow";
 import "./App.css";
 
 class App extends Component {
@@ -9,16 +8,39 @@ class App extends Component {
     this.state = {
       rows: 4,
       cols: 3,
-      currColor: "blue",
+      currColor: "white",
     };
   }
+  //increase rows by 1
+  handleAddRow = () => {
+    this.setState({ rows: this.state.rows + 1 });
+  };
+  //increase columns by 1
+  handleAddColumn = () => {
+    this.setState({ cols: this.state.cols + 1 });
+  };
+  //set color in state
+  handleColorChange = (e) => {
+    this.setState({ currColor: e.target.value });
+  };
   render() {
     return (
-      <Table
-        rows={this.state.rows}
-        cols={this.state.cols}
-        color={this.state.currColor}
-      />
+      <div>
+        <select onChange={this.handleColorChange}>
+          <option value="white">White</option>
+          <option value="black">Black</option>
+          <option value="green">Green</option>
+          <option value="blue">Blue</option>
+          <option value="red">Red</option>
+        </select>
+        <button onClick={this.handleAddRow}>Add Row</button>
+        <button onClick={this.handleAddColumn}>Add Column</button>
+        <Table
+          rows={this.state.rows}
+          cols={this.state.cols}
+          color={this.state.currColor}
+        />
+      </div>
     );
   }
 }
--- src/components/Table.js
+++ src/components/Table.js
@@ -4,28 +4,29 @@ import TableRow from "./TableRow";
 class Table extends Component {
   constructor(props) {
     super(props);
-    // Array of TableRows
+
     this.state = {
-      tablerows: new Array(this.props.rows).fill(
-        <TableRow length={this.props.cols} color={this.props.color} />
-      ),
+      rows: this.props.rows,
+      cols: this.props.cols,
+      color: this.props.color,
     };
   }
 
-  displayEachRow = () => {
-    return (
-      <div className="tablerows">
-        {this.state.tablerows.map((tablerow, index) => (
-          <div className="tablerow" key={index}>
-            {tablerow}
-          </div>
-        ))}
-      </div>
-    );
-  };
-
   render() {
-    return this.displayEachRow;
+    let tablerows = [],
+      i = 0;
+    for (i; i < this.state.rows; i++) {
+      tablerows.push(
+        <div>
+          <TableRow length={this.state.cols} color={this.state.color} />
+        </div>
+      );
+    }
+    return (
+      <table>
+        <tbody>{tablerows}</tbody>
+      </table>
+    );
   }
 }
 export default Table;
