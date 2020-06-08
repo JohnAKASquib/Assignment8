ed566352cb67e1c5ac0e1456d2dac46c29310a38
John Stendardo <johnakasquib96@gmail.com>
2020-06-08 17:51:09 -0400

    Updates

---
+++ precache-manifest.879d988dd2456e918a4dc6f1ab77f69d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6b987cbbf9ed87482bc9163afb3a109",
+    "url": "/Assignment8/index.html"
+  },
+  {
+    "revision": "42ef5c230ff232c99ccc",
+    "url": "/Assignment8/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e3a074420cf5f885288f",
+    "url": "/Assignment8/static/js/2.5ec09ff7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Assignment8/static/js/2.5ec09ff7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42ef5c230ff232c99ccc",
+    "url": "/Assignment8/static/js/main.872f8599.chunk.js"
+  },
+  {
+    "revision": "21edd5b0a261dfcffa81",
+    "url": "/Assignment8/static/js/runtime-main.a4803a0f.js"
+  }
+]);
